02b92b9146ef8efe8bf502fbbc1eecc9b5534ec5
Michael Buesch <m@bues.ch>
2016-04-24 21:41:22 +0200

    phy-serial: Implement basic rx
    Signed-off-by: Michael Buesch <m@bues.ch>

--- pyprofibus/phy_serial.py
+++ pyprofibus/phy_serial.py
@@ -8,8 +8,11 @@
 #
 
 from pyprofibus.phy import *
+from pyprofibus.fdl import FdlError, FdlTelegram
 
 import sys
+import time
+import binascii
 
 try:
 	import serial
@@ -39,12 +42,51 @@ class CpPhySerial(CpPhy):
 		if self.__serial:
 			self.__serial.close()
 			self.__serial = None
+			self.__rxBuf = b""
 		super(CpPhySerial, self).close()
 
 	# Poll for received packet.
 	# timeout => In seconds. 0 = none, Negative = unlimited.
 	def poll(self, timeout = 0):
-		pass#TODO
+		ret, s, size = None, self.__serial, -1
+		timeoutStamp = time.clock() + timeout
+		while True:
+			if len(self.__rxBuf) == size:
+				ret = self.__rxBuf
+				self.__rxBuf = b""
+				break
+
+			if timeout >= 0 and\
+			   time.clock() >= timeoutStamp:
+				break
+
+			if len(self.__rxBuf) < 1:
+				self.__rxBuf += s.read(1)
+				continue
+
+			if len(self.__rxBuf) < 3:
+				try:
+					size = FdlTelegram.getSizeFromRaw(self.__rxBuf)
+				except FdlError:
+					self.__rxBuf += s.read(3 - len(self.__rxBuf))
+				else:
+					self.__rxBuf += s.read(size - len(self.__rxBuf))
+				continue
+
+			if len(self.__rxBuf) >= 3:
+				try:
+					size = FdlTelegram.getSizeFromRaw(self.__rxBuf)
+				except FdlError:
+					self.__rxBuf = b""
+					raise PhyError("RX: Failed to get telegram size")
+				if len(self.__rxBuf) < size:
+					self.__rxBuf += s.read(size - len(self.__rxBuf))
+				continue
+		if self.debug:
+			if ret:
+				print("PHY-serial: received %s" %\
+				      binascii.b2a_hex(ret).decode())
+		return ret
 
 	def setConfig(self, baudrate = CpPhy.BAUD_19200):
 		self.close()
@@ -55,16 +97,21 @@ class CpPhySerial(CpPhy):
 				bytesize = 8,
 				parity = serial.PARITY_EVEN,
 				stopbits = serial.STOPBITS_ONE,
-				timeout = 1.0,
+				timeout = 0.001,
 				xonxoff = False,
 				rtscts = False,
 				dsrdtr = False)
+			self.__rxBuf = b""
 		except serial.SerialException as e:
 			raise PhyError("Failed to set CP-PHY "
 				"configuration:\n" + str(e))
 
 	def profibusSend_SDN(self, telegramData):
-		self.__serial.write(bytearray(telegramData))
+		telegramData = bytearray(telegramData)
+		if self.debug:
+			print("PHY-serial: sending %s" %\
+			      binascii.b2a_hex(telegramData).decode())
+		self.__serial.write(telegramData)
 
 	def profibusSend_SRD(self, telegramData):
 		self.profibusSend_SDN(telegramData)
