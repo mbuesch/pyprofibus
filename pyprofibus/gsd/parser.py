15f68362da0690b01c573a858ec8ad87b96aca4e
Michael Buesch <m@bues.ch>
2016-05-17 20:52:53 +0200

    gsd: Simplify parser
    Signed-off-by: Michael Buesch <m@bues.ch>

--- pyprofibus/gsd/parser.py
+++ pyprofibus/gsd/parser.py
@@ -23,17 +23,6 @@ class GsdParser(object):
 	"""GSD file/data parser.
 	"""
 
-	_baudrates = (("9.6", 9600),
-		      ("19.2", 19200),
-		      ("45.45", 45450),
-		      ("93.75", 93750),
-		      ("187.5", 187500),
-		      ("500", 500000),
-		      ("1.5M", 1500000),
-		      ("3M", 3000000),
-		      ("6M", 6000000),
-		      ("12M", 12000000))
-
 	class _Line(object):
 		"""Raw line.
 		"""
@@ -111,55 +100,7 @@ class GsdParser(object):
 		self.__parse(text)
 
 	def __reset(self):
-		self.__gsdRevision = None
-		self.__vendorName = None
-		self.__modelName = None
-		self.__revision = None
-		self.__identNumber = None
-		self.__slaveFamily = None
-		self.__protocolIdent = None
-		self.__stationType = None
-		self.__hardwareRelease = None
-		self.__softwareRelease = None
-		self.__autoBaudSupp = None
-		self.__baudSupp = {}
-		self.__maxTsdr = {}
-		for baudStr, baud in self._baudrates:
-			self.__baudSupp[baud] = None
-			self.__maxTsdr[baud] = None
-		self.__redundancy = None
-		self.__repeaterCtrlSig = None
-		self.__prmText = []
-		self.__extUserPrmData = []
-		self.__24vPins = None
-		self.__implementationType = None
-		self.__bitmapDevice = None
-		self.__bitmapSF = None
-		self.__orderNumber = None
-		self.__periphery = None
-		self.__s7HeaderCnf = None
-		self.__isActive = None
-		self.__onlyNormalModules = None
-		self.__diagBufferable = None
-		self.__offsetFirstMPDBlock = None
-		self.__ETERDelay = None
-		self.__maxResponseDelay = None
-		self.__failSafe = None
-		self.__minSlaveIntervall = None
-		self.__maxDiagDataLen = None
-		self.__modulOffset = None
-		self.__modularStation = None
-		self.__maxModule = None
-		self.__maxInputLen = None
-		self.__maxOutputLen = None
-		self.__maxDataLen = None
-		self.__userPrmDataLen = None
-		self.__maxUserPrmDataLen = None
-		self.__userPrmData = None
-		self.__extUserPrmDataConst = []
-		self.__extUserPrmDataRef = []
-		self.__fixPresetModules = None
-		self.__module = []
+		self.__fields = {}
 
 	def __preprocess(self, text):
 		lines = [ self._Line(i + 1, l)
@@ -318,178 +259,72 @@ class GsdParser(object):
 		return (offset, data) if hasOffset else data
 
 	def __parseLine_global(self, line):
-		value = self.__trySimpleNum(line, "GSD_Revision")
-		if value is not None:
-			self.__gsdRevision = value
-			return
+		# Parse simple numbers.
+		for name in ("GSD_Revision", "Ident_Number",
+			     "Protocol_Ident", "Station_Type",
+			     "Repeater_Ctrl_Sig", "24V_Pins",
+			     "S7HeaderCnf", "OffsetFirstMPDBlock",
+			     "ETERDelay", "MaxResponseDelay",
+			     "Min_Slave_Intervall", "Max_Diag_Data_Len",
+			     "Modul_Offset", "Max_Module",
+			     "Max_Input_Len", "Max_Output_Len",
+			     "Max_Data_Len", "MaxTsdr_9.6", "MaxTsdr_19.2",
+			     "MaxTsdr_45.45", "MaxTsdr_93.75", "MaxTsdr_187.5",
+			     "MaxTsdr_500", "MaxTsdr_1.5M", "MaxTsdr_3M",
+			     "MaxTsdr_6M", "MaxTsdr_12M", "User_Prm_Data_Len",
+			     "Max_User_Prm_Data_Len"):
+			value = self.__trySimpleNum(line, name)
+			if value is not None:
+				self.__fields[name] = value
+				return
+
+		# Parse simple booleans.
+		for name in ("Freeze_Mode_supp", "Sync_Mode_supp",
+			     "Set_Slave_Add_supp", "Redundancy",
+			     "IsActive", "OnlyNormalModules",
+			     "DiagBufferable", "Fail_Safe",
+			     "Modular_Station", "Auto_Baud_supp",
+			     "9.6_supp", "19.2_supp", "45.45_supp",
+			     "93.75_supp", "187.5_supp", "500_supp",
+			     "1.5M_supp", "3M_supp", "6M_supp",
+			     "12M_supp", "FixPresetModules"):
+			value = self.__trySimpleBool(line, name)
+			if value is not None:
+				self.__fields[name] = value
+				return
+
+		# Parse simple strings.
+		for name in ("Vendor_Name", "Model_Name",
+			     "Revision", "Hardware_Release",
+			     "Software_Release", "Implementation_Type",
+			     "Bitmap_Device", "Bitmap_SF",
+			     "OrderNumber", "Periphery"):
+			value = self.__trySimpleStr(line, name)
+			if value is not None:
+				self.__fields[name] = value
+				return
+
+		# Parse specials
 		value = self.__trySimpleNum(line, "PrmText")
 		if value is not None:
-			self.__prmText.append(self._PrmText(value))
+			self.__fields.setdefault("PrmText", []).append(
+				self._PrmText(value))
 			self.__state = self._STATE_PRMTEXT
-			return
-		value = self.__trySimpleStr(line, "Vendor_Name")
-		if value is not None:
-			self.__vendorName = value
-			return
-		value = self.__trySimpleStr(line, "Model_Name")
-		if value is not None:
-			self.__modelName = value
-			return
-		value = self.__trySimpleStr(line, "Revision")
-		if value is not None:
-			self.__revision = value
-			return
-		value = self.__trySimpleNum(line, "Ident_Number")
-		if value is not None:
-			self.__identNumber = value
 			return
 		value = self.__tryStrNoQuotes(line, "Slave_Family")
 		if value is not None:
-			self.__slaveFamily = value.split("@")
-			return
-		value = self.__trySimpleNum(line, "Protocol_Ident")
-		if value is not None:
-			self.__protocolIdent = value
-			return
-		value = self.__trySimpleNum(line, "Station_Type")
-		if value is not None:
-			self.__stationType = value
-			return
-		value = self.__trySimpleStr(line, "Hardware_Release")
-		if value is not None:
-			self.__hardwareRelease = value
-			return
-		value = self.__trySimpleStr(line, "Software_Release")
-		if value is not None:
-			self.__softwareRelease = value
-			return
-		value = self.__trySimpleBool(line, "Redundancy")
-		if value is not None:
-			self.__redundancy = value
-			return
-		value = self.__trySimpleNum(line, "Repeater_Ctrl_Sig")
-		if value is not None:
-			self.__repeaterCtrlSig = value
-			return
-		value = self.__trySimpleNum(line, "24V_Pins")
-		if value is not None:
-			self.__24vPins = value
-			return
-		value = self.__trySimpleStr(line, "Implementation_Type")
-		if value is not None:
-			self.__implementationType = value
-			return
-		value = self.__trySimpleStr(line, "Bitmap_Device")
-		if value is not None:
-			self.__bitmapDevice = value
-			return
-		value = self.__trySimpleStr(line, "Bitmap_SF")
-		if value is not None:
-			self.__bitmapSF = value
-			return
-		value = self.__trySimpleStr(line, "OrderNumber")
-		if value is not None:
-			self.__orderNumber = value
-			return
-		value = self.__trySimpleStr(line, "Periphery")
-		if value is not None:
-			self.__periphery = value
-			return
-		value = self.__trySimpleNum(line, "S7HeaderCnf")
-		if value is not None:
-			self.__s7HeaderCnf = value
-			return
-		value = self.__trySimpleBool(line, "IsActive")
-		if value is not None:
-			self.__isActive = value
-			return
-		value = self.__trySimpleBool(line, "OnlyNormalModules")
-		if value is not None:
-			self.__onlyNormalModules = value
-			return
-		value = self.__trySimpleBool(line, "DiagBufferable")
-		if value is not None:
-			self.__diagBufferable = value
-			return
-		value = self.__trySimpleNum(line, "OffsetFirstMPDBlock")
-		if value is not None:
-			self.__offsetFirstMPDBlock = value
-			return
-		value = self.__trySimpleNum(line, "ETERDelay")
-		if value is not None:
-			self.__ETERDelay = value
-			return
-		value = self.__trySimpleNum(line, "MaxResponseDelay")
-		if value is not None:
-			self.__maxResponseDelay = value
-			return
-		value = self.__trySimpleBool(line, "Fail_Safe")
-		if value is not None:
-			self.__failSafe = value
-			return
-		value = self.__trySimpleNum(line, "Min_Slave_Intervall")
-		if value is not None:
-			self.__minSlaveIntervall = value
-			return
-		value = self.__trySimpleNum(line, "Max_Diag_Data_Len")
-		if value is not None:
-			self.__maxDiagDataLen = value
-			return
-		value = self.__trySimpleNum(line, "Modul_Offset")
-		if value is not None:
-			self.__modulOffset = value
-			return
-		value = self.__trySimpleBool(line, "Modular_Station")
-		if value is not None:
-			self.__modularStation = value
-			return
-		value = self.__trySimpleNum(line, "Max_Module")
-		if value is not None:
-			self.__maxModule = value
-			return
-		value = self.__trySimpleNum(line, "Max_Input_Len")
-		if value is not None:
-			self.__maxInputLen = value
-			return
-		value = self.__trySimpleNum(line, "Max_Output_Len")
-		if value is not None:
-			self.__maxOutputLen = value
-			return
-		value = self.__trySimpleNum(line, "Max_Data_Len")
-		if value is not None:
-			self.__maxDataLen = value
-			return
-		value = self.__trySimpleNum(line, "Auto_Baud_supp")
-		if value is not None:
-			self.__autoBaudSupp = value
-			return
-		for baudStr, baud in self._baudrates:
-			value = self.__trySimpleBool(line, "%s_supp" % baudStr)
-			if value is not None:
-				self.__baudSupp[baud] = value
-				return
-			value = self.__trySimpleNum(line, "MaxTsdr_%s" % baudStr)
-			if value is not None:
-				self.__maxTsdr[baud] = value
-				return
-		value = self.__trySimpleNum(line, "User_Prm_Data_Len")
-		if value is not None:
-			self.__userPrmDataLen = value
-			return
-		value = self.__trySimpleNum(line, "Max_User_Prm_Data_Len")
-		if value is not None:
-			self.__maxUserPrmDataLen = value
+			self.__fields["Slave_Family"] = value.split("@")
 			return
 		value = self.__tryByteArray(line, "User_Prm_Data")
 		if value is not None:
-			self.__userPrmData = value
+			self.__fields["User_Prm_Data"] = value
 			return
 		m = self._reExtUserPrmData.match(line.text)
 		if m:
 			try:
 				refNr = self.__parseNum(m.group(1))
 				name = m.group(2)
-				self.__extUserPrmData.append(
+				self.__fields.setdefault("ExtUserPrmData", []).append(
 					self._ExtUserPrmData(refNr, name))
 				self.__state = self._STATE_EXTUSERPRMDATA
 			except ValueError as e:
@@ -498,18 +333,14 @@ class GsdParser(object):
 		offset, value = self.__tryByteArray(line, "Ext_User_Prm_Data_Const",
 						    hasOffset = True)
 		if value is not None:
-			self.__extUserPrmDataConst.append(
+			self.__fields.setdefault("Ext_User_Prm_Data_Const", []).append(
 				self._ExtUserPrmDataConst(offset, value))
 			return
 		offset, value = self.__trySimpleNum(line, "Ext_User_Prm_Data_Ref",
 						    hasOffset = True)
 		if value is not None:
-			self.__extUserPrmDataRef.append(
+			self.__fields.setdefault("Ext_User_Prm_Data_Ref", []).append(
 				self._ExtUserPrmDataRef(offset, value))
-			return
-		value = self.__trySimpleBool(line, "FixPresetModules")
-		if value is not None:
-			self.__fixPresetModules = value
 			return
 		m = self._reModule.match(line.text)
 		if m:
@@ -517,7 +348,7 @@ class GsdParser(object):
 				name = m.group(1)
 				config = m.group(2)
 				configBytes = self.__parseByteArray(config)
-				self.__module.append(
+				self.__fields.setdefault("Module", []).append(
 					self._Module(name, configBytes))
 				self.__state = self._STATE_MODULE
 			except ValueError as e:
