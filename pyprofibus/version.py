118d1dbc0654e76b9c9c240981d31841743f1a9d
Michael Buesch <m@bues.ch>
2016-06-04 17:53:21 +0200

    version: Add VERSION_STRING
    Signed-off-by: Michael Buesch <m@bues.ch>

--- pyprofibus/version.py
+++ pyprofibus/version.py
@@ -1,2 +1,7 @@
+from __future__ import division, absolute_import, print_function, unicode_literals
+
+
 VERSION_MAJOR	= 1
 VERSION_MINOR	= 0
+
+VERSION_STRING = "%d.%d" % (VERSION_MAJOR, VERSION_MINOR)
