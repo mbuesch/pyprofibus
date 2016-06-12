8dde2b388a60d10aa6465b134cc48cf09716de4c
Michael Buesch <m@bues.ch>
2016-06-12 17:32:40 +0200

    example/dummy: Add run time measurement
    Signed-off-by: Michael Buesch <m@bues.ch>

--- example_dummy.py
+++ example_dummy.py
@@ -4,33 +4,31 @@
 # This example can be run without any PB hardware.
 #
 
+import sys
 import pyprofibus
 import pyprofibus.phy_dummy
-from pyprofibus import ProfibusError
+from pyprofibus import ProfibusError, monotonic_time
 from pyprofibus.gsd.interp import GsdInterp
 from pyprofibus.dp import DpTelegram_SetPrm_Req
 
-
-# Enable verbose debug messages?
-debug = True
 
 master = None
 try:
 	# Parse the GSD file.
 	# And select the plugged modules.
-	gsd = GsdInterp.fromFile("dummy.gsd", debug = debug)
+	gsd = GsdInterp.fromFile("dummy.gsd", debug = False)
 	gsd.setConfiguredModule("dummy output module")
 	gsd.setConfiguredModule("dummy output module")
 	gsd.setConfiguredModule("dummy input module")
 
 	# Create a PHY (layer 1) interface object
-	phy = pyprofibus.phy_dummy.CpPhyDummySlave(debug = debug)
+	phy = pyprofibus.phy_dummy.CpPhyDummySlave(debug = False)
 	phy.setConfig(19200)
 
 	# Create a DP class 1 master with DP address 1
 	master = pyprofibus.DPM1(phy = phy,
 				 masterAddr = 2,
-				 debug = debug)
+				 debug = True)
 
 	# Create a slave description.
 	slaveDesc = pyprofibus.DpSlaveDesc(identNumber = gsd.getIdentNumber(),
@@ -61,10 +59,21 @@ try:
 	# Run the slave state machine.
 	master.initialize()
 	outData = bytearray( (0x42, 0x24,) )
+	rtSum, runtimes, nextPrint = 0, [ 0, ] * 512, monotonic_time() + 1.0
 	while 1:
+		start = monotonic_time()
+
 		inData = master.runSlave(slaveDesc, outData)
 		if inData is not None:
 			outData = bytearray( (inData[1], inData[0]) )
+
+		end = monotonic_time()
+		runtimes.append(end - start)
+		rtSum = rtSum - runtimes.pop(0) + runtimes[-1]
+		if end > nextPrint:
+			nextPrint = end + 3.0
+			sys.stderr.write("pyprofibus cycle time = %.3f ms\n" %\
+				(rtSum / len(runtimes) * 1000.0))
 
 except ProfibusError as e:
 	print("Terminating: %s" % str(e))
