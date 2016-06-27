baa6bf68d4cab22c2d52b73b915c6400e884f0d2
Michael Buesch <m@bues.ch>
2016-06-27 21:09:17 +0200

    Bump version
    Signed-off-by: Michael Buesch <m@bues.ch>

--- pyprofibus/version.py
+++ pyprofibus/version.py
@@ -2,6 +2,6 @@ from __future__ import division, absolute_import, print_function, unicode_litera
 
 
 VERSION_MAJOR	= 1
-VERSION_MINOR	= 1
+VERSION_MINOR	= 2
 
 VERSION_STRING = "%d.%d" % (VERSION_MAJOR, VERSION_MINOR)
