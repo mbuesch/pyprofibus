6a850abc5bc11206f98af04731546e60d31b40f1
Michael Buesch <m@bues.ch>
2016-07-12 20:45:55 +0200

    Add more assertion helpers
    Signed-off-by: Michael Buesch <m@bues.ch>

--- tests/pyprofibus_tstlib.py
+++ tests/pyprofibus_tstlib.py
@@ -7,3 +7,7 @@ import pyprofibus.phy_serial
 import nose as __nose
 
 assert_eq = __nose.tools.assert_equal
+assert_lt = __nose.tools.assert_less
+assert_le = __nose.tools.assert_less_equal
+assert_gt = __nose.tools.assert_greater
+assert_ge = __nose.tools.assert_greater_equal
