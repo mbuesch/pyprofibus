9bd3dcdb174423c76895402af3be5197d5ba9262
Michael Buesch <m@bues.ch>
2019-04-06 15:25:23 +0200

    Move fpga driver
    Signed-off-by: Michael Buesch <m@bues.ch>

--- pyprofibus/phy_fpga_driver/__init__.py
+++ pyprofibus/phy_fpga_driver/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import division, absolute_import, print_function, unicode_literals
 from pyprofibus.compat import *
 
-from pyprofibus.fpga.driver import *
+from pyprofibus.phy_fpga_driver.driver import *
