0ae5da4e5983db6d00332cd5f083c5c8376bf54d
Michael Buesch <m@bues.ch>
2019-05-29 19:56:52 +0200

    phy-fpga: Add missing import
    Signed-off-by: Michael Buesch <m@bues.ch>

--- pyprofibus/phy_fpga_driver/messages.py
+++ pyprofibus/phy_fpga_driver/messages.py
@@ -11,6 +11,8 @@
 from __future__ import division, absolute_import, print_function, unicode_literals
 from pyprofibus.compat import *
 
+from pyprofibus.phy_fpga_driver.exceptions import *
+
 
 __all__ = [
 	"FpgaPhyMsg",
