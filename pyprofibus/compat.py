b344932a90e88786d1e278025c472cb1ad6cf482
Michael Buesch <m@bues.ch>
2020-07-19 19:06:27 +0200

    compat: Remove unused reduce()
    Signed-off-by: Michael Buesch <m@bues.ch>

--- pyprofibus/compat.py
+++ pyprofibus/compat.py
@@ -40,11 +40,6 @@ if isPy2Compat:
 if isPy2Compat:
 	range = xrange
 
-# reduce() compatibility.
-# Force Python2 behavior
-if isPy3Compat:
-	from functools import reduce
-
 # BlockingIOError dummy
 try:
 	BlockingIOError
