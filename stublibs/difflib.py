de7d6af02b037ea9b6b5452eff75b30880fb38ae
Michael Buesch <m@bues.ch>
2020-07-24 23:15:55 +0200

    stub/difflib: Add trivial get_close_matches() implementation
    Signed-off-by: Michael Buesch <m@bues.ch>

--- stublibs/difflib.py
+++ stublibs/difflib.py
@@ -1,2 +1,7 @@
 def get_close_matches(word, possibilities, n=3, cutoff=0.6):
-	raise NotImplementedError
+	def fold(s):
+		return s.lower().replace(" ", "").replace("\t", "")
+	for p in possibilities:
+		if fold(p) == fold(word):
+			return [p]
+	return []
