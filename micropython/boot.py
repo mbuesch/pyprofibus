7fd44b7d4f8d38dcccdd142ac598dd152bb91837
Michael Buesch <m@bues.ch>
2020-08-01 13:04:35 +0200

    micropython: Enable gc after allocation
    Signed-off-by: Michael Buesch <m@bues.ch>

--- micropython/boot.py
+++ micropython/boot.py
@@ -1,4 +1,9 @@
 print("boot.py")
 import sys, gc
+
+# Add stubs to Python path.
 sys.path.insert(0, "/stublibs")
+
+# Enable gc after allocation of this many bytes:
+gc.threshold(2**11)
 gc.collect()
