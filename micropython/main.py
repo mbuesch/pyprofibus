4848877ec88b0e443eb7ef074a244a7db55cfa40
Michael Buesch <m@bues.ch>
2020-08-06 21:21:57 +0200

    micropython/main: Catch all exceptions in all cases
    Signed-off-by: Michael Buesch <m@bues.ch>

--- micropython/main.py
+++ micropython/main.py
@@ -1,21 +1,47 @@
 print("main.py")
+
+
+# Start the pyprofibus application.
+# Modify this to your needs.
+# This function should never return. If it does nevertheless, a reboot is attempted.
+def start_pyprofibus():
+	print("Starting pyprofibus...")
+
+	# Start dummy example that uses virtual bus hardware.
+	import example_dummy
+	example_dummy.main() # Run main loop
+	return
+
+	# Start the S7-312-2DP example.
+	#import example_s7_315_2dp
+	#example_s7_315_2dp.main() # Run main loop
+	#return
+
+	# Start the ET200S example.
+	#import example_et200s
+	#example_et200s.main() # Run main loop
+	#return
+
+
+# Main execution loop.
+# This runs start_pyprofibus and does its best to catch exceptions.
+count = 0
 while True:
 	try:
-		print("Starting pyprofibus...")
+		count += 1
 		import gc
 		gc.collect()
-
-		# Start dummy example that uses virtual bus hardware.
-		import example_dummy
-		example_dummy.main()
-
-		# Start the S7-312-2DP example.
-		#import example_s7_315_2dp
-		#example_s7_315_2dp.main()
-
-		# Start the ET200S example.
-		#import example_et200s
-		#example_et200s.main()
-
+		start_pyprofibus()
+	except KeyboardInterrupt as e:
+		raise e
 	except Exception as e:
-		print("FATAL: " + str(e))
+		try:
+			print("FATAL exception: " + str(e))
+		except: pass
+	except: pass
+	try:
+		if count >= 5:
+			count = 0
+			import machine
+			machine.reset()
+	except: pass
