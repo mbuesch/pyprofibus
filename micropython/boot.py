853b8acd6d6bed781c173b34cb7ebde31939d0da
Michael Buesch <m@bues.ch>
2020-09-10 20:01:35 +0200

    micropython: Add switch to disable watchdog
    Signed-off-by: Michael Buesch <m@bues.ch>

--- micropython/boot.py
+++ micropython/boot.py
@@ -1,8 +1,14 @@
-# Start the watchdog first.
-import machine
-watchdog = machine.WDT(timeout=5000).feed
-
 print("boot.py")
+
+# Start the watchdog first.
+if 1:
+	import machine
+	watchdog = machine.WDT(timeout=5000).feed
+	print("Watchdog active.")
+else:
+	watchdog = None
+	print("Watchdog inactive.")
+
 import sys, gc
 
 # Add stubs to Python path.
