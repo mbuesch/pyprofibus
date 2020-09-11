e891e9335bc1c789aefaf7621788045df6c1e544
Michael Buesch <m@bues.ch>
2020-09-11 20:10:55 +0200

    Export PhyError
    Signed-off-by: Michael Buesch <m@bues.ch>

--- pyprofibus/__init__.py
+++ pyprofibus/__init__.py
@@ -2,4 +2,5 @@ from pyprofibus.conf import PbConf, PbConfError
 from pyprofibus.dp import DpError
 from pyprofibus.dp_master import DPM1, DPM2, DpSlaveDesc
 from pyprofibus.fdl import FdlError
+from pyprofibus.phy import PhyError
 from pyprofibus.util import ProfibusError
